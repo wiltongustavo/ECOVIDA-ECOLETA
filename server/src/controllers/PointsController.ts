3ccaa4dfee8cafd4bfbb0eb866ca182359e5bc55
wiltongustavo <44711690+wiltongustavo@users.noreply.github.com>
2020-06-07 19:49:16 -0300

    Update PointsController.ts

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -55,7 +55,7 @@ class PointsController{
         const trx = await knex.transaction();
 
         const point = {
-            
+                    // a inserção desta imagem fake esta dando problema no momento que tentar realizar um cadastro de ponto de coleta, deixo marcado para atualização futuramente.
                 image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
                  name,
                  email,
